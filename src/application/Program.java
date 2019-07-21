bbc17eee4327d495b95141d6e9530025969fbb20
jandercl <j.a.n.d.e.r.c.l.a.d@gmail.com>
2019-07-21 20:53:52 -0300

    Mais atualizações

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Worker;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com  nome do Departamento: ");
+		String departament = sc.nextLine();
+		System.out.print("Entre com os dados do trabalhador: ");
+		System.out.print("Nome: ");
+		String workerName = sc.nextLine();
+		System.out.print("Nível: ");
+		String workerLevel = sc.nextLine();
+		System.out.print("Salario base: ");
+		double baseSalary = sc.nextDouble();
+		
+		Worker worker = new 
+		
+		
+		
 						
 	}
 	
